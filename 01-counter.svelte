15f31430647a75e94459454165f9cd116e32412b
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-02-26 21:45:39 +0300

    Create 01-counter.svelte

---
+++ 01-counter.svelte
@@ -0,0 +1,25 @@
+<!--App.svelte file-->
+
+<script>
+
+  let counter = 0
+
+  let counterUp = () => {counter += 1}
+
+  let counterDown = () => {counter -= 1}
+
+</script>
+
+<main>
+  <h5>Mustafa Buyukdereli created this counter:)</h5>
+
+  <button on:click={counterUp}>Counter ++</button>
+
+  <input type="number" bind:value={counter}/>
+
+  <button on:click={counterDown}>Counter --</button>
+</main>
+
+<style>
+
+</style>
