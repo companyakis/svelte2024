23c499bab1b296afff185925e70970391d66217f
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-02-28 23:07:03 +0300

    Create 02-02-App.svelte

---
+++ 02-02-App.svelte
@@ -0,0 +1,19 @@
+<!--App.svelte file-->
+
+<script>
+
+  import Header from "./components/Header.svelte";
+  import Welcome from "./components/Welcome.svelte";
+  import Footer from "./components/Footer.svelte";
+
+</script>
+
+<Header />
+
+<Welcome />
+
+<Footer />
+
+<style>
+
+</style>
