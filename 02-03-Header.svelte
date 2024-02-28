073be476617d5fbb376cc650129d1f6028bbae50
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-02-28 23:10:46 +0300

    Create 02-03-Header.svelte

---
+++ 02-03-Header.svelte
@@ -0,0 +1,16 @@
+<!--Header.svelte file-->
+
+<script>
+  import SignInButton from "./SignInButton.svelte";
+</script>
+
+<div>
+  <h1>Innovation is Calling Us</h1>
+  <SignInButton />
+  <!--Nested component usage-->
+  <ul>
+    <li>DeFi</li>
+    <li>AI</li>
+    <li>Algorithmic Trading</li>
+  </ul>
+</div>
