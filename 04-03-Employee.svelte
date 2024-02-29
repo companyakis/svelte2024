b99ded70c4ff98376a513faf62a48e0dd881b1fd
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-02-29 11:23:54 +0300

    Create 04-03-Employee.svelte

---
+++ 04-03-Employee.svelte
@@ -0,0 +1,14 @@
+<!--Employee.svelte file-->
+
+<script>
+    export let name
+    export let title
+    export let about
+</script>
+
+
+<div>
+  <h2>Name: {name}</h2>
+  <h3>Title: {title}</h3>
+  <p>About: {about}</p>
+</div>
