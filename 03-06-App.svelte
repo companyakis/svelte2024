724324c905b89c65eb3658399860bd0641c1c3ff
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-02-29 10:21:58 +0300

    Create 03-06-App.svelte

---
+++ 03-06-App.svelte
@@ -0,0 +1,28 @@
+<!--App.svelte file-->
+
+<!--props and default value-->
+
+<script>
+  import Calculations from "./components/Calculations.svelte";
+
+  let counter = 0;
+
+  const increment = () => {
+    counter += 5;
+  };
+
+  const decrement = () => {
+    counter -= 5;
+  };
+</script>
+
+<button on:click={increment}>Up</button>
+
+<Calculations {counter} />
+
+<Calculations /> <!--default value-->
+
+<button on:click={decrement}>Down</button>
+
+<style>
+</style>
