3f9db6f5249ef2dbcc1f79a86b431da77f5f4fcd
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-02-29 11:22:27 +0300

    Create 04-02-App.svelte

---
+++ 04-02-App.svelte
@@ -0,0 +1,26 @@
+<!--App.svelte file-->
+
+<!--spread props-->
+
+<script>
+  import Employee from "./components/Employee.svelte";
+
+  const employeeInfo1 = {
+    name: "Aygün Göğebakan",
+    title: "CFO",
+    about: "Finance expert over 10 years"
+  }
+
+  const employeeInfo2 = {
+    name: "Kültigin Göktürk",
+    title: "CEO",
+    about: "Management expert over 25 years"
+  }
+</script>
+
+<Employee {...employeeInfo1} />
+
+<Employee {...employeeInfo2}/>
+
+<style>
+</style>
