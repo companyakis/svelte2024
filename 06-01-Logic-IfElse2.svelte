3fb59768ebcde6053fb54066e0e1e54720f14c9d
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-03-05 21:38:22 +0300

    Create 06-01-Logic-IfElse2.svelte

---
+++ 06-01-Logic-IfElse2.svelte
@@ -0,0 +1,27 @@
+<!--App.svelte file-->
+
+<script>
+
+  let condition = false
+
+  let toogleButton = () => {
+    condition = !condition
+  }
+  
+</script>
+
+{#if !condition}
+
+  <button on:click={toogleButton}>User Login</button>
+
+  {:else} 
+  <button on:click={toogleButton}>User Logout</button>
+
+{/if}
+
+
+
+<style>
+
+
+</style>
