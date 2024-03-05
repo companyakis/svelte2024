430fcb2826ba3b22dd4543ed0e2f0057f19a06c3
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-03-05 22:29:15 +0300

    Create 06-02-Logic-Each1.svelte

---
+++ 06-02-Logic-Each1.svelte
@@ -0,0 +1,18 @@
+<!--Employee.svelte file-->
+
+<script>
+  export let name
+  export let department
+  export let wage
+</script>
+
+
+<div>
+
+  <ul>
+    <li>{name}</li>
+    <li>{department}</li>
+    <li>{wage}</li>
+  </ul>
+
+</div>
