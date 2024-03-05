2a0b657c496a5199e2e4d846111ac0c70ec8661e
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-03-05 22:43:39 +0300

    Create 06-02-Logic-Each2.svelte

---
+++ 06-02-Logic-Each2.svelte
@@ -0,0 +1,28 @@
+<!--App.svelte file-->
+
+<script>
+
+  import Employee from "./components/Employee.svelte";
+
+  let employees = [
+    {id: 1, name: "Mustafa Büyükdereli", department: "Future Innovations", wage: "$10000"},
+    {id: 2, name: "Aygün Kaplan", department: "Finance Operations", wage: "$3000"},
+    {id: 3, name: "John Nash", department: "RD", wage: "$8000"},
+  ]
+
+
+</script>
+
+{#each employees as e (e.id) }
+
+  <Employee {...e}/>
+
+  <!-- <Employee name={e.name} department={e.department} wage={e.wage}/> -->
+
+{/each}
+
+
+<style>
+
+
+</style>
