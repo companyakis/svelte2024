414cfba1f4030e090d1da3cd59570296ce2d7259
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-03-05 20:00:46 +0300

    Create 05-01-ReactivityCounterApp.svelte

---
+++ 05-01-ReactivityCounterApp.svelte
@@ -0,0 +1,24 @@
+<!--App.svelte file-->
+
+<!--$: variableName -->
+
+<script>
+
+  let counter = 0
+
+  $: info = counter
+
+  const handleClick = () => {
+    counter = counter * counter + 1
+  }
+  
+</script>
+
+<button on:click={handleClick}>Counter: {counter}</button>
+
+<p>Counter info: {info}</p>
+
+<style>
+
+
+</style>
