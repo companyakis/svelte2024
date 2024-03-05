6a99e48e1fbab81c6a2d234dc9991b2b1a1da4cf
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-03-05 19:47:58 +0300

    Create 05-01-Reactivity.svelte

---
+++ 05-01-Reactivity.svelte
@@ -0,0 +1,25 @@
+<!--App.svelte file-->
+
+<!--$: variableName -->
+
+<script>
+  let name = "Mustafa"
+
+  let year = 2024
+
+  $: greeting = "Welcome " + name + " and " + year + " will be your year!"
+  
+</script>
+
+<input type="text" bind:value={name}>
+
+<h1>Name: {name}</h1>
+
+<h3>Year: {year}</h3>
+
+<h2>{greeting}</h2>
+
+<style>
+
+
+</style>
